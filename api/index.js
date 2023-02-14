dd16696ca28dcdbd903f258d1d84f44e0251c94c
Sergiy Antonyuk <s.p.antonyuk@gmail.com>
2023-02-14 23:33:13 +0100

    airbnb-omega-three.vercel.app

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
 app.use(cors({
   credentials: true,
-  origin: 'https://airbnb-git-main-sxidsvit.vercel.app/',
+  origin: 'airbnb-omega-three.vercel.app',
 }));
 
 mongoose.set('strictQuery', true)
