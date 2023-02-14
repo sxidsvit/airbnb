f77b15367b24370207e93c1a5e2492ff3b136585
Sergiy Antonyuk <s.p.antonyuk@gmail.com>
2023-02-15 00:27:05 +0100

    https://airbnb-second.vercel.app

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
 app.use(cors({
   credentials: true,
-  origin: 'airbnb-second.vercel.app',
+  origin: 'https://airbnb-second.vercel.app',
 }));
 
 mongoose.set('strictQuery', true)
